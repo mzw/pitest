f9fb327abe37cac4b19408214e4f3415d27c8f5c
Yuta Maezawa <maezawa@nii.ac.jp>
2017-08-07 17:40:54 +0900

    enable to log PIT outputs

--- pitest/src/main/java/jp/mzw/adamu/CLI.java
+++ pitest/src/main/java/jp/mzw/adamu/CLI.java
@@ -2,8 +2,10 @@ package jp.mzw.adamu;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.logging.LogManager;
 
 import org.apache.maven.shared.invoker.MavenInvocationException;
 
@@ -16,34 +18,41 @@ import jp.mzw.adamu.core.AdaMu;
  *
  */
 public class CLI {
-     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, MavenInvocationException {
-          if (args.length == 0) {
-               System.err.println("jp.mzw.adamu.CLI <command> <arguments>");
-               System.exit(-1);
-          }
-          String command = args[0];
+	public static void main(String[] args)
+			throws IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, MavenInvocationException {
+		if (args.length == 0) {
+			System.err.println("jp.mzw.adamu.CLI <command> <arguments>");
+			System.exit(-1);
+		}
+		String command = args[0];
 
-          if ("run".equals(command)) {
-               String[] rargs = Arrays.copyOfRange(args, 1, args.length);
-               CLI.run(rargs);
-          } else if ("quit".equals(command)) {
-               String[] rargs = Arrays.copyOfRange(args, 1, args.length);
-               CLI.quit(rargs);
-          }
-     }
-     
-     private static void run(String[] args) throws IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, MavenInvocationException {
-          if (args.length < 2) {
-               System.err.println("jp.mzw.adamu.CLI run <path to subject directory> <mutation operator set> <(optional) enabled>");
-               System.exit(-1);
-          }
-          boolean enabled = args.length == 2 ? true : args[2].equals("disabled") ? false : true;
-          AdaMu adamu = new AdaMu(new File(args[0]), AdaMu.MutationOperatorSet.valueOf(args[1].toUpperCase()), enabled);
-          adamu.run();
-     }
-     
-     private static void quit(String[] args) throws ClassNotFoundException, SQLException {
-          Class.forName("org.sqlite.JDBC");
-          Stats.getInstance().insert(Stats.Label.Quit, System.currentTimeMillis());
-     }
+		InputStream is = CLI.class.getClassLoader().getResourceAsStream("logging.properties");
+		if (is != null) {
+			LogManager.getLogManager().readConfiguration(is);
+		}
+
+		if ("run".equals(command)) {
+			String[] rargs = Arrays.copyOfRange(args, 1, args.length);
+			CLI.run(rargs);
+		} else if ("quit".equals(command)) {
+			String[] rargs = Arrays.copyOfRange(args, 1, args.length);
+			CLI.quit(rargs);
+		}
+	}
+
+	private static void run(String[] args)
+			throws IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, MavenInvocationException {
+		if (args.length < 2) {
+			System.err.println("jp.mzw.adamu.CLI run <path to subject directory> <mutation operator set> <(optional) enabled>");
+			System.exit(-1);
+		}
+		boolean enabled = args.length == 2 ? true : args[2].equals("disabled") ? false : true;
+		AdaMu adamu = new AdaMu(new File(args[0]), AdaMu.MutationOperatorSet.valueOf(args[1].toUpperCase()), enabled);
+		adamu.run();
+	}
+
+	private static void quit(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		Stats.getInstance().insert(Stats.Label.Quit, System.currentTimeMillis());
+	}
 }
